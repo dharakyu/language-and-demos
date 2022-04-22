8d42fda475d1871f51310322622d5dd988904b9d
Dhara Yu <dharakyu@node13-ccncluster.stanford.edu>
2022-04-21 22:40:55 -0700

    simple game with expected reward loss working

--- code/game.py
+++ code/game.py
@@ -1,33 +1,168 @@
 import numpy as np
 import torch
 
-class SignalingBanditsGame():
+class SimpleGame():
     def __init__(self, num_choices=3):
         self.num_choices = num_choices
 
     def sample_utilities(self):
-        utilities = np.array([1, 9, 10], dtype=np.float32)
-        #utilities = np.random.choice(10, size=self.num_choices, replace=False) + 1
-        #utilities = np.zeros(shape=(3,))
-        #idx = np.random.choice(3)
-        #utilities[idx] = 1
+        """
+        Randomly select utilities for a single game
+
+        Return:
+        utilities: np.array of size (num_choices,)
+        """
+        utilities = np.random.choice(10, size=self.num_choices, replace=False) + 1
         return utilities.astype(np.float32) / utilities.max()
 
-        return utilities.astype(np.float32)
-
     def generate_batch(self, batch_size=32):
+        """
+        Generate games for a batch
+
+        Return:
+        batch: np.array of size (batch_size, num_choices)
+        """
         batch = []
         for _ in range(batch_size):
             batch.append(self.sample_utilities())
-
         return np.array(batch)
 
-    def compute_rewards(
-        self, utilities, choices
-    ):
+    def compute_rewards(self, utilities, choices):
         """
         Given a batch of utilities and agent choices, compute the rewards for each agent.
+
+        Return
+        rewards: np.array of size (batch_size,)
         """
         rewards = utilities.gather(-1, choices)
-
         return rewards
+
+
+class SignalingBanditsGame():
+    def __init__(self, num_choices=3, 
+                    num_colors=3, num_shapes=3, 
+                    max_color_val=2, max_shape_val=1
+                ):
+        self.num_choices = num_choices
+        self.num_colors = num_colors
+        self.num_shapes = num_shapes
+
+        self.color_utilities = np.linspace(start=-max_color_val, stop=max_color_val, num=num_colors)
+        self.shape_utilities = np.linspace(start=-max_shape_val, stop=max_shape_val, num=num_shapes)
+
+        self.embeddings, self.rewards = self.get_reward_matrix()
+        self.reward_matrix = np.concatenate((self.embeddings, self.rewards), dim=1)
+        self.embedding2reward = zip(self.embeddings, self.rewards)
+
+    def get_reward_matrix(self):
+        """
+        Generate the reward matrix, which the speaker will gain access to
+
+        Return:
+        reward_matrix: np.array of size(self.num_colors*self.num_shapes, self.num_colors + self.num_shapes + 1)
+        i.e. if the item described by the feature [0, 1, 0, 1, 0, 0] has utility 3, then the corresponding row in the matrix
+        is [0, 1, 0, 1, 0, 0, 3]
+        """
+        embeddings = []
+        rewards = []
+        for i in range(self.num_colors):
+            for j in range(self.num_shapes):
+                color_embedding = np.zeros(shape=(self.num_colors,))
+                color_embedding[i] = 1
+                color_utility = self.color_utilities[i]
+
+                shape_embedding = np.zeros(shape=(self.num_shapes,))
+                shape_embedding[j] = 1
+                shape_utility = self.shape_utilities[j]
+
+                embedding = np.concatenate((color_embedding, shape_embedding))
+
+                embeddings.append(embedding)
+                rewards.append(color_utility + shape_utility)
+
+        return np.array(embeddings), np.array(rewards)
+
+    def sample_game(self):
+        """
+        Sample a single game
+
+        Return:
+        game: np.array of size (num_choices, self.num_colors + self.num_shapes)
+        """
+        num_objects = self.num_colors * self.num_shapes
+        indices = np.random.choice(num_objects, size=self.num_choices, replace=False)
+
+        game = self.reward_matrix[indices, -1]  # lop off the last element bc that's the reward
+        return game
+
+    def sample_batch(self, batch_size=32):
+        """
+        Sample games for a whole batch
+
+        Return
+        batch_games: np.array of size(batch_size, self.num_choices, self.num_colors + self.num_shapes)
+        """
+        batch_games = np.array(shape=(batch_size, self.num_choices, self.num_colors + self.num_shapes))
+        for i in range(batch_size):
+            game = self.sample_game()
+            batch_games[i, :, :] = game
+
+        return batch_games
+
+    def compute_reward(self, games, preds):
+        """
+        Given a batch of games and model prediction, compute the rewards
+
+        Return:
+        accuracy: np.array of size (batch_size)
+        """
+        batch_size = games.shape[0]
+        batch_reward = np.array((batch_size))
+
+        for i in range(batch_size):
+            game_utilities = games[i]
+            game_rewards = np.array((self.num_choices,))
+            for j in range(self.num_choices):
+                game_rewards[j] = self.embedding2reward[game_utilities[j]]
+
+            prediction_idx = preds[i]
+            predicted_object = games[i, prediction_idx, :]
+            reward = self.embedding2reward[predicted_object]
+            reward_normalized = reward / game_rewards.max()
+
+            batch_reward[i] = reward_normalized
+
+        return batch_reward
+
+    def get_ground_truth(self, games):
+        """
+        Given a batch of games, return the indices of the object in each game that 
+        has the highest associated utility
+
+        Return:
+        ground_truth: np.array of size (batch_size)
+        """
+        batch_size = games.shape[0]
+        ground_truth = np.array((batch_size))
+        
+        for i in range(batch_size):
+            game_utilities = games[i]
+            game_rewards = np.array((self.num_choices,))
+            for j in range(self.num_choices):
+                game_rewards[j] = self.embedding2reward[game_utilities[j]]
+            
+            idx = np.argmax(game_rewards)
+            ground_truth[i] = idx
+
+        return ground_truth
+            
+
+            
+
+
+
+
+
+
+
+    
--- code/train.py
+++ code/train.py
@@ -1,6 +1,6 @@
 from speaker import Speaker
 from listener import Listener
-from game import SignalingBanditsGame
+from game import SimpleGame, SignalingBanditsGame
 
 import torch
 import torch.nn as nn
@@ -13,7 +13,7 @@ from statistics import mean
 
 lr = 1e-4
 num_epochs = 1000
-num_batches_per_epoch = 20
+num_batches_per_epoch = 100
 
 def compute_stats(utilities, choices, rewards):
     #breakpoint()
@@ -27,7 +27,8 @@ def run_epoch(split, speaker, listener, optimizer, loss_func):
     batch_losses = []
     batch_accuracy = []
     for _ in range(num_batches_per_epoch):
-        game = SignalingBanditsGame()
+        game = SimpleGame()
+        
         utilities = game.generate_batch()
         utilities = torch.from_numpy(utilities)
 
@@ -40,17 +41,15 @@ def run_epoch(split, speaker, listener, optimizer, loss_func):
 
         #rf_loss = -(log_probs * rewards).mean()
         #loss = rf_loss
-        model_choices = torch.argmax(logits, dim=-1)
+        preds = torch.argmax(logits, dim=-1)
         ground_truth = torch.argmax(utilities, dim=-1)
 
-        rewards = game.compute_rewards(utilities, model_choices.unsqueeze(-1))
-        avg_reward = compute_stats(utilities, model_choices, rewards)
-        accuracy = (ground_truth == model_choices).float().mean().item()
+        rewards = game.compute_rewards(utilities, preds.unsqueeze(-1))
+        avg_reward = compute_stats(utilities, preds, rewards)
+        accuracy = (ground_truth == preds).float().mean().item()
 
-        #breakpoint()
-        #choices_one_hot = F.one_hot(choices).float()
-        #ground_truth_one_hot = F.one_hot(ground_truth).float()
-        loss = loss_func(logits, ground_truth)
+        # multiply by -1 bc we are maximizing the expected reward which means minimizing the negative of that
+        loss = -1 * torch.bmm(logits.unsqueeze(1), utilities.unsqueeze(-1)).squeeze().sum()
         #breakpoint()
 
         if training:
@@ -65,7 +64,7 @@ def run_epoch(split, speaker, listener, optimizer, loss_func):
     print('avg reward', mean(batch_rewards))
     print('train loss', mean(batch_losses))
     print('accuracy', mean(batch_accuracy))
-    print('choices', model_choices.float().mean().item())
+    print('predictions', preds.float().mean().item())
 
 def main():
     s = Speaker()
@@ -79,7 +78,6 @@ def main():
 
     for i in range(num_epochs):
         print('epoch', i)
-        if i > 10: breakpoint()
         run_epoch('train', s, l, optimizer, loss_func)
         run_epoch('val', s, l, optimizer, loss_func)
 
